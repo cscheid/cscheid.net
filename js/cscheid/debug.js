dd14c55b49ec02981586e8c25cb2a71a3a2346da
Carlos Scheidegger <cscheid@cs.arizona.edu>
2017-11-03 10:24:02 -0700

    more code for the site

--- js/cscheid/debug.js
+++ js/cscheid/debug.js
@@ -23,4 +23,24 @@
         trace = [];
     }
     debug.clear = clear;
+
+    https://stackoverflow.com/questions/1382107/whats-a-good-way-to-extend-error-in-javascript
+    function UnrecoverableError(message) {
+        this.name = 'UnrecoverableError';
+        this.message = message;
+        this.stack = (new Error()).stack;
+    }
+    UnrecoverableError.prototype = new Error;
+    debug.UnrecoverableError = UnrecoverableError;
+    
+    function die(msg) {
+        throw new UnrecoverableError(msg);
+    }
+    debug.die = assert;
+    
+    function assert(cond, msg) {
+        if (!cond)
+            die(msg);
+    }
+    debug.assert = assert;
 })();
