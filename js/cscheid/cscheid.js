9f1e6c12ad7ff2828863db5c4b8db7d9ac4a8541
Carlos Scheidegger <cscheid@cs.arizona.edu>
2018-09-28 08:42:32 -0700

    update

--- js/cscheid/cscheid.js
+++ js/cscheid/cscheid.js
@@ -1,1 +1,27 @@
-cscheid = {};
+import * as approximation from "./cscheid/approximation.js";
+import * as basis         from "./cscheid/basis.js";
+import * as caliper       from "./cscheid/caliper.js";
+import * as data          from "./cscheid/data.js";
+import * as debug         from "./cscheid/debug.js";
+import * as dom           from "./cscheid/dom.js";
+import * as geometry      from "./cscheid/geometry.js";
+import * as math          from "./cscheid/math.js";
+import * as plot          from "./cscheid/plot.js";
+import * as random        from "./cscheid/random.js";
+import * as svg           from "./cscheid/svg.js";
+import * as time          from "./cscheid/time.js";
+
+export { approximation,
+         basis,
+         caliper,
+         data,
+         debug,
+         dom,
+         geometry,
+         math,
+         plot,
+         random,
+         svg,
+         time };
+
+
