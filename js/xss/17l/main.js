a3066971047b9fb854615482b9b99b63fca9be4f
Carlos Scheidegger <cscheid@cscheid.net>
2021-05-31 11:33:17 -0700

    goodies

--- js/xss/17l/main.js
+++ js/xss/17l/main.js
@@ -1,3 +1,5 @@
+/*global addScript, d3*/
+
 // https://www.evanmiller.org/how-not-to-sort-by-average-rating.html
 function ciLowerBound(wins, trials) // at 95% confidence
 {
@@ -32,10 +34,24 @@ function extractLeaderboardData()
   return data;
 }
 
+// https://stackoverflow.com/questions/3665115/how-to-create-a-file-in-memory-for-user-to-download-but-not-through-server
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
 addScript("https://d3js.org/d3.v6.js")
   .then(() => {
     console.log("17 lands xss!");
-    let data = extractLeaderboardData();
   });
 
 
